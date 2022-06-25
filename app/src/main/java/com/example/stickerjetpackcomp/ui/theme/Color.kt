88ba6b0be8bf3a82417c63e85d96f117d64bcbbe
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2022-06-25 12:02:43 +0100

    Update Color.kt

--- app/src/main/java/com/example/stickerjetpackcomp/ui/theme/Color.kt
+++ app/src/main/java/com/example/stickerjetpackcomp/ui/theme/Color.kt
@@ -24,10 +24,49 @@ val color11 = Color(0xFFf0932b)
 val color12 = Color(0xFF006266)
 val color13 = Color(0xFF6F1E51)
 
-val colors= listOf(color1,color2,color3,color4,color5,color6,color7,color8,color9,color10,
+
+val colors = listOf(
+    color1, color2, color3, color4, color5, color6,
+    color7,
+    color8,
+    color9,
+    color10,
+    color11,
+    color12,
+    color13,
+    color7,
+    color8,
+    color9,
+    color10,
+    color11,
+    color12,
+    color13,
+    color8,
+    color9,
+    color10,
+    color11,
+    color12,
+    color13,
+    color7,
+    color8,
+    color9,
+    color10,
+    color11,
+    color12,
+    color13,
+    color8,
+    color9,
+    color10,
+    color11,
+    color12,
+    color13,
+    color7,
+    color8,
+    color9,
+    color10,
     color11,
     color12,
     color13
 
-    )
+)
 
